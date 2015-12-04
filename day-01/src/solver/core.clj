eb6ab25506d445af4f34f5e980616b7dcdbd51a6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-04 15:36:58 +0100

    Solve first puzzle
    You can find the answer by running the command `lein run`

--- day-01/src/solver/core.clj
+++ day-01/src/solver/core.clj
@@ -1,7 +1,12 @@
 (ns solver.core
   (:gen-class))
 
+(defn determine-floor [word]
+  "Determines which floor Santa should deliver presents"
+  (let [freq (frequencies word)]
+    (- (freq \()) (freq \))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (determine-floor (slurp "input"))))
