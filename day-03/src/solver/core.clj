f94cbc79ef049ccfacaaba2d5e8564fb96f9eeaf
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-06 08:28:12 +0100

    Solve day 3 puzzle
    You can view the solution with the `lein run` command

--- day-03/src/solver/core.clj
+++ day-03/src/solver/core.clj
@@ -1,7 +1,31 @@
 (ns solver.core
+  (:require clojure.set clojure.string)
   (:gen-class))
+
+(defn move-to [[x y] [dx dy]] [(+ x dx) (+ y dy)])
+
+(defn to-direction [character] (case character
+                                 ">" [ 1  0]
+                                 "<" [-1  0]
+                                 "^" [ 0  1]
+                                 "v" [ 0 -1]))
+
+(defn reducer [data direction]
+  (let [next-current (move-to (data :current) direction)
+        next-data { :visited (clojure.set/union (data :visited) #{next-current})
+                   :current next-current}]
+    next-data))
+
+(defn visited-houses [description]
+  (let [steps (clojure.string/split description #"")
+        directions (map to-direction steps)
+        initial { :visited #{ [0 0] } :current [0 0] }]
+    (reduce reducer initial directions)))
+
+(defn count-visited-houses [description]
+  (count ((visited-houses description) :visited)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (count-visited-houses (slurp "input"))))
