7a2a1bfa070c3fd4bf76186ca36c62268ced50a7
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-07 16:34:35 +0100

    Solve part two of day 1 puzzle

--- day-01/src/solver/core.clj
+++ day-01/src/solver/core.clj
@@ -1,16 +1,26 @@
 (ns solver.core
   (:gen-class))
 
+(defn- to-direction [index direction] { :index index :direction direction })
+
 (defn determine-floor [xs]
-  (let [to-direction (fn [index direction] {:index index :direction direction})
+  (let [
         determine-floor (fn [floor data] (+ floor  (case (data :direction)
                                              "(" +1
                                              ")" -1)))]
     (reduce determine-floor 0  (map-indexed to-direction xs))))
 
+(defn first-basement [directions]
+  (let [to-floor (fn [direction] (assoc direction
+                                        :floor (determine-floor
+                                                (take (direction :index) directions))))
+        hit-basement (fn [data] (= (data :floor) -1))]
+    (first (filter hit-basement (map to-floor (map-indexed to-direction directions))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [directions (clojure.string/split (slurp "input") #"")]
-    (println (determine-floor directions))))
+    (println (determine-floor directions))
+    (println (first-basement directions))))
 
