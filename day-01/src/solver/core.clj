4118ee32dfbd2bdc08df96bf8973f4a9fb7692fb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-07 16:15:40 +0100

    Use instructions to calculate floor

--- day-01/src/solver/core.clj
+++ day-01/src/solver/core.clj
@@ -1,11 +1,14 @@
 (ns solver.core
   (:gen-class))
 
-(defn determine-floor [word]
-  "Determines which floor Santa should deliver presents"
-  (let [freqs (frequencies word)] (- (get freqs \( 0) (get freqs \) 0))))
+(defn determine-floor [xs]
+  (let [to-direction (fn [index direction] {:index index :direction direction})
+        determine-floor (fn [floor data] (+ floor  (case (data :direction)
+                                             "(" +1
+                                             ")" -1)))]
+    (reduce determine-floor 0  (map-indexed to-direction xs))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (determine-floor (slurp "input"))))
+  (println (determine-floor (clojure.string/split (slurp "input") #""))))
