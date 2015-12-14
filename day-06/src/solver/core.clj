ae412343755864abd8f4af03e79aed44adbe99b1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-14 13:29:48 +0100

    Create a grid of lights

--- day-06/src/solver/core.clj
+++ day-06/src/solver/core.clj
@@ -1,6 +1,14 @@
 (ns solver.core
   (:gen-class))
 
+(defn create-grid
+  ([] (create-grid 1000))
+  ([w] (create-grid w w))
+  ([w h] (let [row (vec (map (fn [x] false) (range w)))
+               grid (vec (map (fn [x] row) (range h)))]
+           grid)))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
