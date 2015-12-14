e9b7220dd44717ac0aa56da4f700b7504bb56c4d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2015-12-14 16:49:08 +0100

    Act on a grid

--- day-06/src/solver/core.clj
+++ day-06/src/solver/core.clj
@@ -8,6 +8,22 @@
                grid (vec (map (fn [x] row) (range h)))]
            grid)))
 
+(defn act-on
+  ([f row a b] (let [low a
+                     high (+ b 1)
+                     difference (- high low)]
+                 (vec (concat
+                       (take low row)
+                       (map f (take difference (drop low row)))
+                       (drop high row)))))
+  ([f grid a b c d] (let [low a
+                          high (+ c 1)
+                          difference (- high low)
+                          act-on-row (fn [row] (act-on f row b d))]
+                      (vec (concat
+                            (take low grid)
+                            (map act-on-row (take difference (drop low grid)))
+                            (drop high grid))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
